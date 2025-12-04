c35aa9125e3d457012a95e2f8eb4848d61da7349
jszabala19-bit <jszabala19@gmail.com>
2025-12-04 16:46:49 +0800

    Delete Zabala - PRLM Hands - On Test1 directory

--- Zabala - PRLM Hands - On Test1/footer.php
+++
@@ -1,3 +0,0 @@
-<footer> 
-    <p>Created by: Jan Chrysler Zabala ©2025</p>
-</footer>
--- Zabala - PRLM Hands - On Test1/header.php
+++
@@ -1,7 +0,0 @@
-<header> 
-    <h1>Maskulado's Shoe Apparel by Jc</h1>
-    <nav>
-        <a href="index.php">Home</a>
-        <a href="everydayshoes.php">Product</a>
-    </nav>
-</header>
